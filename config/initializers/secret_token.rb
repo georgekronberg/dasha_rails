bbb107f797804e5a9f78200c1162ca5e09c6c185
George Kronberg <rails@georgekronberg.com>
2012-01-29 14:26:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DashaRails::Application.config.secret_token = '6be0901ac76e13167edbbbcf37139d04f178c7a1b0759a2fd48aabbc22585c479eb70ea8b2c06f717ea1a8da9bf7c2c636f3870efc3bd220c532a62fe0f482e5'
